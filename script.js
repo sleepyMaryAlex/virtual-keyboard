8322e2bebdd176269c35bf3b3387c528a197b147
Maria Guchkova <mariaguchkova1@gmail.com>
2022-05-08 17:48:33 +0300

    fix: remove eslint errors

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-import i18Obj from "./translate.js";
+import i18Obj from './translate.js';
 
-const DEFAULT_LANG = "en";
-let language = localStorage.getItem("language") || DEFAULT_LANG;
+const DEFAULT_LANG = 'en';
+let language = localStorage.getItem('language') || DEFAULT_LANG;
 
 let isCapsLocked = false;
 
 let cursorPosition = 0;
-let textareaText = "";
+let textareaText = '';
 
-const body = document.querySelector("body");
+const body = document.querySelector('body');
 
 function showContent() {
   const content = `<div class="wrapper">
@@ -96,183 +96,168 @@ function showContent() {
                         </div>
                     </div>`;
 
-  body.insertAdjacentHTML("afterbegin", content);
+  body.insertAdjacentHTML('afterbegin', content);
 }
 showContent();
 
-const data = document.querySelectorAll("[data-i18]");
-const letters = document.querySelectorAll(".letter");
-const digits = document.querySelectorAll(".digit");
-const symbols = document.querySelectorAll(".symbol");
-const shifts = document.querySelectorAll(".shift");
-getTranslate();
+const data = document.querySelectorAll('[data-i18]');
+const letters = document.querySelectorAll('.letter');
+const digits = document.querySelectorAll('.digit');
+const symbols = document.querySelectorAll('.symbol');
+const shifts = document.querySelectorAll('.shift');
 
-const controlLeft = document.querySelector("#ControlLeft");
-const altLeft = document.querySelector("#AltLeft");
-const controlRight = document.querySelector("#ControlRight");
-const altRight = document.querySelector("#AltRight");
+const controlLeft = document.querySelector('#ControlLeft');
+const altLeft = document.querySelector('#AltLeft');
+const controlRight = document.querySelector('#ControlRight');
+const altRight = document.querySelector('#AltRight');
 
-const textarea = document.querySelector(".textarea");
-const keyboard = document.querySelector(".keyboard");
-
-document.addEventListener("keydown", pressKey);
-document.addEventListener("keyup", releaseKey);
-
-function pressKey(e) {
-  let key = document.querySelector(`#${e.code}`);
-  if (key) {
-    if (key.classList.contains("key")) {
-      e.preventDefault();
-    }
-    if (key.classList.contains("control-key")) {
-      key.classList.add("active-background");
-    }
-    key.classList.add("active");
-    if (key.classList.contains("shift")) {
-      shiftSymbols();
-      changeLetters();
-    }
-    determinePressedKey(key);
-  }
-  if (
-    controlLeft.classList.contains("active") &&
-    altLeft.classList.contains("active")
-  ) {
-    changeLanguage();
-  }
-  if (
-    controlRight.classList.contains("active") &&
-    altRight.classList.contains("active")
-  ) {
-    changeLanguage();
-  }
-  if (
-    controlRight.classList.contains("active") &&
-    altLeft.classList.contains("active")
-  ) {
-    changeLanguage();
-  }
-  if (
-    controlLeft.classList.contains("active") &&
-    altRight.classList.contains("active")
-  ) {
-    changeLanguage();
-  }
-}
-
-function releaseKey(e) {
-  let key = document.querySelector(`#${e.code}`);
-  if (key) {
-    if (key.classList.contains("control-key") && !key.classList.contains("capslock")) {
-      key.classList.remove("active-background");
-    }
-    if (!key.classList.contains("capslock")) {
-        key.classList.remove("active");
-    }
-    determineUpKey(key);
-  }
-}
-
-function changeLanguage() {
-  if (language === "ru") {
-    language = "en";
-  } else if (language === "en") {
-    language = "ru";
-  }
-  saveToLocalStorage();
-  if (isCapsLocked) {
-    data.forEach(
-      (elem) =>
-        (elem.textContent = i18Obj[language][elem.dataset.i18].toUpperCase())
-    );
-  } else if (!isCapsLocked) {
-    data.forEach(
-      (elem) =>
-        (elem.textContent = i18Obj[language][elem.dataset.i18].toLowerCase())
-    );
-  }
-}
+const textarea = document.querySelector('.textarea');
+const keyboard = document.querySelector('.keyboard');
 
 function saveToLocalStorage() {
-  localStorage.setItem("language", language);
+  localStorage.setItem('language', language);
 }
 
 function getTranslate() {
-  data.forEach(
-    (elem) => (elem.textContent = i18Obj[language][elem.dataset.i18])
-  );
+  data.forEach((el) => {
+    const elem = el;
+    elem.textContent = i18Obj[language][elem.dataset.i18];
+  });
 }
 
-keyboard.addEventListener("click", identifyKey);
-
-shifts.forEach((shift) =>
-  shift.addEventListener("mousedown", () => {
-    shiftSymbols();
-    changeLetters();
-  })
-);
-
-shifts.forEach((shift) =>
-  shift.addEventListener("mouseup", (e) => {
-    let key = e.target;
-    determineUpKey(key);
-  })
-);
-
-function identifyKey(e) {
-  let key = e.target;
-  determinePressedKey(key);
-}
-
-function determinePressedKey(key) {
-  if (key.classList.contains("key") && !key.classList.contains("control-key")) {
-    enterText(key.textContent);
-  }
-  if (key.classList.contains("arrow")) {
-    enterText(key.textContent);
-  }
-  if (key.classList.contains("capslock")) {
-    toUpperAndLowerCase(key);
-  }
-  if (key.classList.contains("enter")) {
-    enterText("\n");
-  }
-  if (key.classList.contains("tab")) {
-    enterText("\t");
-  }
-  if (key.classList.contains("backspace")) {
-    deleteFromTextarea();
-  }
-  if (key.classList.contains("del")) {
-    deleteFromTextareaByDel();
-  }
-  updateTextarea();
-}
-
-function determineUpKey(key) {
-  if (key.classList.contains("shift")) {
-    if (!isCapsLocked) {
-      letters.forEach(
-        (letter) => (letter.textContent = letter.textContent.toLowerCase())
-      );
-    } else if (isCapsLocked) {
-      letters.forEach(
-        (letter) => (letter.textContent = letter.textContent.toUpperCase())
-      );
-    }
-    unshiftSymbols();
+function shiftSymbols() {
+  const [one, two, three, four, five, six, seven, eight, nine, zero] = digits;
+  const [
+    backquote,
+    minus,
+    equal,
+    bracketleft,
+    bracketright,
+    backslash,
+    semicolon,
+    quote,
+    comma,
+    period,
+    slash,
+  ] = symbols;
+  if (language === 'en') {
+    one.textContent = '!';
+    two.textContent = '@';
+    three.textContent = '#';
+    four.textContent = '$';
+    five.textContent = '%';
+    six.textContent = '^';
+    seven.textContent = '&';
+    eight.textContent = '*';
+    nine.textContent = '(';
+    zero.textContent = ')';
+    backquote.textContent = '~';
+    minus.textContent = '_';
+    equal.textContent = '+';
+    bracketleft.textContent = '{';
+    bracketright.textContent = '}';
+    backslash.textContent = '|';
+    semicolon.textContent = ':';
+    quote.textContent = '"';
+    comma.textContent = '<';
+    period.textContent = '>';
+    slash.textContent = '?';
+  } else if (language === 'ru') {
+    one.textContent = '!';
+    two.textContent = '"';
+    three.textContent = '№';
+    four.textContent = ';';
+    five.textContent = '%';
+    six.textContent = ':';
+    seven.textContent = '?';
+    eight.textContent = '*';
+    nine.textContent = '(';
+    zero.textContent = ')';
+    minus.textContent = '_';
+    equal.textContent = '+';
+    backslash.textContent = '/';
+    slash.textContent = ',';
   }
 }
 
-textarea.addEventListener("click", () => {
+function unshiftSymbols() {
+  const [one, two, three, four, five, six, seven, eight, nine, zero] = digits;
+  const [
+    backquote,
+    minus,
+    equal,
+    bracketleft,
+    bracketright,
+    backslash,
+    semicolon,
+    quote,
+    comma,
+    period,
+    slash,
+  ] = symbols;
+  one.textContent = '1';
+  two.textContent = '2';
+  three.textContent = '3';
+  four.textContent = '4';
+  five.textContent = '5';
+  six.textContent = '6';
+  seven.textContent = '7';
+  eight.textContent = '8';
+  nine.textContent = '9';
+  zero.textContent = '0';
+  minus.textContent = '-';
+  equal.textContent = '=';
+  if (language === 'en') {
+    backquote.textContent = '`';
+    bracketleft.textContent = '[';
+    bracketright.textContent = ']';
+    backslash.textContent = '\\';
+    semicolon.textContent = ';';
+    quote.textContent = "'";
+    comma.textContent = ',';
+    period.textContent = '.';
+    slash.textContent = '/';
+  } else if (language === 'ru') {
+    backslash.textContent = '\\';
+    slash.textContent = '.';
+  }
+}
+
+function changeLetters() {
+  if (isCapsLocked) {
+    letters.forEach((l) => {
+      const letter = l;
+      letter.textContent = letter.textContent.toLowerCase();
+    });
+  } else if (!isCapsLocked) {
+    letters.forEach((l) => {
+      const letter = l;
+      letter.textContent = letter.textContent.toUpperCase();
+    });
+  }
+}
+
+function deleteFromTextarea() {
+  if (cursorPosition > 0) {
+    textareaText = textareaText.substring(0, textarea.selectionStart - 1)
+      + textareaText.substring(textarea.selectionEnd);
+    cursorPosition -= 1;
+  }
+}
+
+function deleteFromTextareaByDel() {
+  textareaText = textareaText.substring(0, textarea.selectionStart)
+    + textareaText.substring(textarea.selectionEnd + 1);
+}
+
+textarea.addEventListener('click', () => {
   cursorPosition = textarea.selectionStart;
 });
 
 function enterText(text) {
-  textareaText =
-    textareaText.substring(0, cursorPosition) +
-    text +
-    textareaText.substring(cursorPosition);
+  textareaText = textareaText.substring(0, cursorPosition)
+    + text
+    + textareaText.substring(cursorPosition);
   cursorPosition += text.length;
 }
 
@@ -282,147 +267,166 @@ function updateTextarea() {
   textarea.focus();
 }
 
-function deleteFromTextarea() {
-  if (cursorPosition > 0) {
-    textareaText =
-      textareaText.substring(0, textarea.selectionStart - 1) +
-      textareaText.substring(textarea.selectionEnd);
-    cursorPosition--;
-  }
-}
-
-function deleteFromTextareaByDel() {
-  textareaText =
-    textareaText.substring(0, textarea.selectionStart) +
-    textareaText.substring(textarea.selectionEnd + 1);
-}
-
 function toUpperAndLowerCase(key) {
   if (isCapsLocked) {
-    key.classList.remove("capslock-active");
-    key.classList.remove("active");
-    key.classList.remove("active-background");
-    letters.forEach(
-      (letter) => (letter.textContent = letter.textContent.toLowerCase())
-    );
+    key.classList.remove('capslock-active');
+    key.classList.remove('active');
+    key.classList.remove('active-background');
+    letters.forEach((l) => {
+      const letter = l;
+      letter.textContent = letter.textContent.toLowerCase();
+    });
     isCapsLocked = false;
   } else if (!isCapsLocked) {
-    letters.forEach(
-      (letter) => (letter.textContent = letter.textContent.toUpperCase())
-    );
-    key.classList.add("capslock-active");
-    key.classList.add("active");
-    key.classList.add("active-background");
+    letters.forEach((l) => {
+      const letter = l;
+      letter.textContent = letter.textContent.toUpperCase();
+    });
+    key.classList.add('capslock-active');
+    key.classList.add('active');
+    key.classList.add('active-background');
     isCapsLocked = true;
   }
 }
 
-function shiftSymbols() {
-  let [one, two, three, four, five, six, seven, eight, nine, zero] = digits;
-  let [
-    backquote,
-    minus,
-    equal,
-    bracketleft,
-    bracketright,
-    backslash,
-    semicolon,
-    quote,
-    comma,
-    period,
-    slash,
-  ] = symbols;
-  if (language === "en") {
-    one.textContent = "!";
-    two.textContent = "@";
-    three.textContent = "#";
-    four.textContent = "$";
-    five.textContent = "%";
-    six.textContent = "^";
-    seven.textContent = "&";
-    eight.textContent = "*";
-    nine.textContent = "(";
-    zero.textContent = ")";
-    backquote.textContent = "~";
-    minus.textContent = "_";
-    equal.textContent = "+";
-    bracketleft.textContent = "{";
-    bracketright.textContent = "}";
-    backslash.textContent = "|";
-    semicolon.textContent = ":";
-    quote.textContent = '"';
-    comma.textContent = "<";
-    period.textContent = ">";
-    slash.textContent = "?";
-  } else if (language === "ru") {
-    one.textContent = "!";
-    two.textContent = '"';
-    three.textContent = "№";
-    four.textContent = ";";
-    five.textContent = "%";
-    six.textContent = ":";
-    seven.textContent = "?";
-    eight.textContent = "*";
-    nine.textContent = "(";
-    zero.textContent = ")";
-    minus.textContent = "_";
-    equal.textContent = "+";
-    backslash.textContent = "/";
-    slash.textContent = ",";
+function changeLanguage() {
+  if (language === 'ru') {
+    language = 'en';
+  } else if (language === 'en') {
+    language = 'ru';
   }
-}
-
-function unshiftSymbols() {
-  let [one, two, three, four, five, six, seven, eight, nine, zero] = digits;
-  let [
-    backquote,
-    minus,
-    equal,
-    bracketleft,
-    bracketright,
-    backslash,
-    semicolon,
-    quote,
-    comma,
-    period,
-    slash,
-  ] = symbols;
-  one.textContent = "1";
-  two.textContent = "2";
-  three.textContent = "3";
-  four.textContent = "4";
-  five.textContent = "5";
-  six.textContent = "6";
-  seven.textContent = "7";
-  eight.textContent = "8";
-  nine.textContent = "9";
-  zero.textContent = "0";
-  minus.textContent = "-";
-  equal.textContent = "=";
-  if (language === "en") {
-    backquote.textContent = "`";
-    bracketleft.textContent = "[";
-    bracketright.textContent = "]";
-    backslash.textContent = "\\";
-    semicolon.textContent = ";";
-    quote.textContent = "'";
-    comma.textContent = ",";
-    period.textContent = ".";
-    slash.textContent = "/";
-  } else if (language === "ru") {
-    backslash.textContent = "\\";
-    slash.textContent = ".";
-  }
-}
-
-function changeLetters() {
+  saveToLocalStorage();
   if (isCapsLocked) {
-    letters.forEach(
-      (letter) => (letter.textContent = letter.textContent.toLowerCase())
-    );
+    data.forEach((el) => {
+      const elem = el;
+      elem.textContent = i18Obj[language][elem.dataset.i18].toUpperCase();
+    });
   } else if (!isCapsLocked) {
-    letters.forEach(
-      (letter) => (letter.textContent = letter.textContent.toUpperCase())
-    );
+    data.forEach((el) => {
+      const elem = el;
+      elem.textContent = i18Obj[language][elem.dataset.i18].toLowerCase();
+    });
   }
 }
+
+function determinePressedKey(key) {
+  if (key.classList.contains('key') && !key.classList.contains('control-key')) {
+    enterText(key.textContent);
+  }
+  if (key.classList.contains('arrow')) {
+    enterText(key.textContent);
+  }
+  if (key.classList.contains('capslock')) {
+    toUpperAndLowerCase(key);
+  }
+  if (key.classList.contains('enter')) {
+    enterText('\n');
+  }
+  if (key.classList.contains('tab')) {
+    enterText('\t');
+  }
+  if (key.classList.contains('backspace')) {
+    deleteFromTextarea();
+  }
+  if (key.classList.contains('del')) {
+    deleteFromTextareaByDel();
+  }
+  updateTextarea();
+}
+
+function pressKey(e) {
+  const key = document.querySelector(`#${e.code}`);
+  if (key) {
+    if (key.classList.contains('key')) {
+      e.preventDefault();
+    }
+    if (key.classList.contains('control-key')) {
+      key.classList.add('active-background');
+    }
+    key.classList.add('active');
+    if (key.classList.contains('shift')) {
+      shiftSymbols();
+      changeLetters();
+    }
+    determinePressedKey(key);
+  }
+  if (
+    controlLeft.classList.contains('active')
+    && altLeft.classList.contains('active')
+  ) {
+    changeLanguage();
+  }
+  if (
+    controlRight.classList.contains('active')
+    && altRight.classList.contains('active')
+  ) {
+    changeLanguage();
+  }
+  if (
+    controlRight.classList.contains('active')
+    && altLeft.classList.contains('active')
+  ) {
+    changeLanguage();
+  }
+  if (
+    controlLeft.classList.contains('active')
+    && altRight.classList.contains('active')
+  ) {
+    changeLanguage();
+  }
+}
+
+function determineUpKey(key) {
+  if (key.classList.contains('shift')) {
+    if (!isCapsLocked) {
+      letters.forEach((l) => {
+        const letter = l;
+        letter.textContent = letter.textContent.toLowerCase();
+      });
+    } else if (isCapsLocked) {
+      letters.forEach((l) => {
+        const letter = l;
+        letter.textContent = letter.textContent.toUpperCase();
+      });
+    }
+    unshiftSymbols();
+  }
+}
+
+function releaseKey(e) {
+  const key = document.querySelector(`#${e.code}`);
+  if (key) {
+    if (
+      key.classList.contains('control-key')
+      && !key.classList.contains('capslock')
+    ) {
+      key.classList.remove('active-background');
+    }
+    if (!key.classList.contains('capslock')) {
+      key.classList.remove('active');
+    }
+    determineUpKey(key);
+  }
+}
+
+document.addEventListener('keydown', pressKey);
+document.addEventListener('keyup', releaseKey);
+
+shifts.forEach((shift) => shift.addEventListener('mousedown', () => {
+  shiftSymbols();
+  changeLetters();
+}));
+
+shifts.forEach((shift) => shift.addEventListener('mouseup', (e) => {
+  const key = e.target;
+  determineUpKey(key);
+}));
+
+function identifyKey(e) {
+  const key = e.target;
+  determinePressedKey(key);
+}
+
+keyboard.addEventListener('click', identifyKey);
+
+getTranslate();
